e894c141f55e8e216261e78208a7ea6e4dc0fa7d
Nayelydaza20 <nayelydaza3@gmail.com>
2021-10-13 18:27:15 -0400

    Se creo la carpeta models y su clase

---
+++ Parcial/Models/carta.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial.Models
+{
+    public class carta
+    {
+        [Key]
+
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "escriba de nuevo")]
+        public string Autor { get; set; }
+        [Required(ErrorMessage = "{0} es requerido")]
+        [StringLength(200, MinimumLength = 10, ErrorMessage = "La longitud de {0} debe estar entre {2} y {1}")]
+        public string Letra { get; set; }
+        [Url]
+        [StringLength(100, MinimumLength = 10, ErrorMessage = "La longitud de {0} debe estar entre {2} y {1}")]
+        public string Enlace { get; set; }
+
+
+    }
+}
