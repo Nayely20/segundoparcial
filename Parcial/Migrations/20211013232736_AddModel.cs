fc657d2e292ff2bb280126f31d6df81d2ec232fd
Nayelydaza20 <nayelydaza3@gmail.com>
2021-10-13 19:28:15 -0400

    Migrations Implementado

---
+++ Parcial/Migrations/20211013232736_AddModel.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Parcial.Migrations
+{
+    public partial class AddModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "carta",
+                columns: table => new
+                {
+                    NaipeId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Imagen = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Enlace = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_carta", x => x.NaipeId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "carta");
+        }
+    }
+}
