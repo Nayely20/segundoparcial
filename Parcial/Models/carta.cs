f5edb2cd2a58af1174abc225acd060d7a7f969cf
Nayelydaza20 <nayelydaza3@gmail.com>
2021-10-13 18:32:16 -0400

    se realizo cambios en el codigo

--- Parcial/Models/carta.cs
+++ Parcial/Models/carta.cs
@@ -6,16 +6,24 @@ using System.Threading.Tasks;
 
 namespace Parcial.Models
 {
-    public class carta
+    public class carta   
+ // NaipeId -> contiene de la A al naipe K
+
+//Nombre -> Nombre de la Carta
+
+
+
+
+
     {
         [Key]
 
+        public string NaipeId { get; set; }
+        [Required(ErrorMessage = "contiene de la A al naipe K")]
         public string Nombre { get; set; }
-        [Required(ErrorMessage = "escriba de nuevo")]
-        public string Autor { get; set; }
         [Required(ErrorMessage = "{0} es requerido")]
         [StringLength(200, MinimumLength = 10, ErrorMessage = "La longitud de {0} debe estar entre {2} y {1}")]
-        public string Letra { get; set; }
+        public string Imagen { get; set; }
         [Url]
         [StringLength(100, MinimumLength = 10, ErrorMessage = "La longitud de {0} debe estar entre {2} y {1}")]
         public string Enlace { get; set; }
